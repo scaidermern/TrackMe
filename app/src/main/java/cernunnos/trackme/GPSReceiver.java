85c0cfc8e6f991937b33e04617e81f99de6ae9bb
Alexander Heinlein <alexander.heinlein@web.de>
2017-07-01 13:39:12 +0200

    GPSReceiver: Fix forced storing/upload of locations

--- app/src/main/java/cernunnos/trackme/GPSReceiver.java
+++ app/src/main/java/cernunnos/trackme/GPSReceiver.java
@@ -300,8 +300,13 @@ public class GPSReceiver extends Service implements LocationListener {
             locationManager.removeUpdates(this);
         }
 
+        // force saving to storage and uploading if
+        // - called from deleteLocations() -> location == null
+        // - asked to obtain a single location only -> !isRecording
+        boolean forceSaving = (location == null) || !isRecording;
+
         // save to storage if save interval reached or called from deleteLocations()
-        saveProgressToStorage(location == null);
+        saveProgressToStorage(forceSaving);
 
         // send broadcast message with location backlog
         sendLocationBroadcast();
@@ -314,7 +319,7 @@ public class GPSReceiver extends Service implements LocationListener {
         }
 
         // trigger ftp upload if upload interval reached or called from deleteLocations()
-        uploadProgress(location == null);
+        uploadProgress(forceSaving);
     }
 
     /** Send broadcast message with location backlog */
